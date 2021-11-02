231f2005a14afb975c9c8db16e118942bd6e8637
joshua-tree-ctrl <55064706+joshua-tree-ctrl@users.noreply.github.com>
2021-11-02 21:38:07 +0000

    adding comment explanations

--- helper.js
+++ helper.js
@@ -1,13 +1,17 @@
+// module.export object which contains queryParamHelper function
 module.exports = {
   queryParamHelper: function (urlPath, name, value, set) {
     let setUrl;
     try {
+      // try and catch, early return in catch if path provided cannot be parsed as new URL.
       setUrl = new URL(urlPath);
     } catch (e) {
+      // log e (error) with string invalid_url_provided
       console.log("INVALID_URL_PROVIDED " + e);
       return e;
     }
     if (set === true) {
+      // if set strictly equals boolean true
       setUrl.searchParams.set(name, value);
     } else {
       setUrl.searchParams.append(name, value);
--- test.js
+++ test.js
@@ -2,18 +2,22 @@ const { expect } = require("chai");
 var chai = require("chai");
 var assert = chai.assert;
 var helper = require("./helper.js");
+// import function from helper file, queryParamHelper function from obj in helper file.
 var queryParamHelper = helper.queryParamHelper;
 
 /* Mocha & Chai */
 
 describe("helperFunctions", function () {
+  // describe sets up a grouping function, followed by another nested grouping. Outer group is helperFunctions followed by nested group of queryParamSet
   describe("queryParamSet", function () {
+    // set up variables for use in beforeEach
     let urlPath;
     let nameSet;
     let value;
     let set;
     let setQueryParamAnswer;
 
+    //beforeEach function runs before each it()function, allowing set up of variables to be passed to it(). Variables need to be declared outside of the beforeEach, but inside the describe(group).
     beforeEach(function () {
       urlPath =
         "https://www.myhelperfunction-set.co.uk/?queryNameSet=queryValueSet";
@@ -24,11 +28,14 @@ describe("helperFunctions", function () {
         "https://www.myhelperfunction-set.co.uk/?queryNameSet=queryValueSetViaFunction";
     });
 
+    // it()function with string description, followed by assertion within function.
+    // This incorporates chai assertions, i.e expect -> to -> equal.
     it("should set the query parameter", function () {
       let isSetParam = queryParamHelper(urlPath, nameSet, value, set);
       expect(isSetParam).to.equal(setQueryParamAnswer);
     });
 
+    // another test case, but with a string as opposed to the urlPath var set to assert an error
     it("throw an invalid URL error", function () {
       let errorSetParam = queryParamHelper(
         "this-isnt-a-website-url",
@@ -40,11 +47,13 @@ describe("helperFunctions", function () {
     });
   });
 
+  //another nested group within the outer group
   describe("queryParamAppend", function () {
     let urlPath;
     let nameSet;
     let value;
 
+    // repeat of above, following append of the function as opposed to set of queryParam
     beforeEach(function () {
       urlPath = "https://www.myhelperfunction-append.co.uk/";
       nameSet = "queryNameAppended";
